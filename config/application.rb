135fd57814e641385ff983624f742f85b5a0d266
Patrick Lewis <p.lewis@manifestech.com>
2014-02-09 22:12:10 -0600

    updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,6 @@ module Reposearch
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += %w( /images/* )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
